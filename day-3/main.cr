c31b888ac3ec9daa97f7971e21be57788dba465d
Cameron Roe <cameron.roe@smartbear.com>
2018-12-03 00:48:48 -0600

    refactored day 3 to make it much faster

--- day-3/main.cr
+++ day-3/main.cr
@@ -1,17 +1,17 @@
-fabric = Hash(Tuple(Int32, Int32), Int32).new do |hash, key|
-    hash[key] = 0
-end
-
-fabric_2 = Hash(Tuple(Int32, Int32), Array(Int32)).new do |hash, key|
-    hash[key] = Array(Int32).new
+fabric = Array.new(1000) do
+    Array.new(1000) do
+        Array(Int32).new
+    end
 end
 
 input = File.open("input")
 claim_regex = /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/
+all_ids = [] of Int32
 input.each_line do |line|
     parse_match = claim_regex.match(line)
     next if parse_match.nil?
     id = parse_match[1].to_i
+    all_ids << id
     left = parse_match[2].to_i
     top = parse_match[3].to_i
     width = parse_match[4].to_i
@@ -19,23 +19,21 @@ input.each_line do |line|
 
     (left...(left + width)).each do |l|
         (top...(top + height)).each do |t|
-            fabric[{l, t}] += 1
-            fabric_2[{l, t}] << id
+            fabric[l][t] << id
         end
     end
 end
 
-square_inches = fabric.values.count { |x| x > 1 }
-
-dup_ids = {} of Int32 => Bool
-fabric_2.values.each do |i|
-    i.each { |j| dup_ids[j] ||= false }
-    if i.size > 1
-        i.each { |j| dup_ids[j] ||= true }
+square_inches = 0
+bad_ids = Set(Int32).new
+fabric.each do |i|
+    i.each do |j|
+        square_inches += 1 if j.size > 1
+        bad_ids = bad_ids.concat(j) if j.size > 1
     end
 end
 
-no_dup = dup_ids.find { |id, has_dup| has_dup == false }
+good_id = (all_ids - bad_ids.to_a).first
 
-puts "part 1: #{square_inches}"
-puts "part 2: #{no_dup[0]}" unless no_dup.nil?
+puts "part 1 full: #{square_inches}"
+puts "part 2 full: #{good_id}"
