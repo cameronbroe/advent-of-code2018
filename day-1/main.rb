69da2c8ec36e737705b21e1cbe3cdb8990fe10db
Cameron Roe <cameron.roe@smartbear.com>
2018-12-03 12:00:06 -0600

    ported day 1 to Ruby

---
+++ day-1/main.rb
@@ -0,0 +1,50 @@
+total = 0
+memoized_totals = {}
+repeated_total = 0
+repeated_found = false
+initial_total = 0
+
+def parse_frequency_change(frequency_change)
+    match_data = /^(\+|\-)(\d+)$/.match(frequency_change)
+    return 0 if match_data.nil?
+    pos_or_neg = match_data[1]
+    number = match_data[2].to_i
+    return number if pos_or_neg == "+"
+    return (-1 * number) if pos_or_neg == "-"
+    0
+end
+
+iteration_count = 1
+
+input = File.open("input")
+freq_changes_memoized = []
+input.each_line do |line|
+    freq_change = parse_frequency_change(line)
+    freq_changes_memoized << freq_change
+    if memoized_totals.has_key?(total)
+        repeated_total = total
+        repeated_found = true
+        break
+    else
+        memoized_totals[total] = true
+    end
+    total += freq_change
+end
+initial_total = total
+
+while !repeated_found
+    iteration_count += 1
+    freq_changes_memoized.each do |freq_change|
+        if memoized_totals.has_key?(total)
+            repeated_total = total
+            repeated_found = true
+            break
+        else
+            memoized_totals[total] = true
+        end
+        total += freq_change
+    end
+end
+
+puts "Total: #{initial_total}"
+puts "First Repeated: #{repeated_total}"
